96343657b2cf69223522c32299d61d2e529da652
TrungKKien <trungkien7802@gmail.com>
2023-05-06 02:35:50 +0700

    V3

--- js/main.js
+++ js/main.js
@@ -1,79 +1,82 @@
-$("#play").click(function () { 
+var test = false
+var random 
+
+//hàm bắt đầu trò chơi
+$("#play").click(function () {
     $(".show").removeClass("show");
     $(".fix").addClass("show");
-    // $('table td').text('');
-    var random = ranDomArr();
-    playGame(random)
-});
-    
-function playGame(random) {
+    $("tbody").empty();
+    $('#mess').text('');
+    if(test){
+        random = 0
+        test = false
+    } 
+    random = ranDomArr();
+    test = true 
     console.log(random)
-    $(document).ready(function () { 
-        $(".number").click(function () {
-            var a = $(this).text() + " ";
-            var check = $(this).text()
-            var b = $("#mess").text().slice(0, 6);
-            var set = $("#mess").text().split(" ");
-            console.log(set)
-            if (set.indexOf(check) !== -1) {
-                alert("Giá trị đã được nhập trước đó. Vui lòng nhập giá trị khác.");
-                $("#mess").html(b);
-            } else {
-                $("#mess").html(b + a);
-            }
-        }); 
-    });
-    $("#submit").click(function () { 
-        var input = $("#mess").text().split(" ");
-        if(input.length > 4){
-            var table = document.getElementById("myTable").getElementsByTagName("tbody")[0];
-            var newRow = table.insertRow();
-            var output =  $("#mess").text().split(",");
-            document.getElementById("mess").innerHTML = ""
-            var dung = 0;
-            var vitri = 0;
-            for(i = 0 ; i < 4 ; i++){
-                for(j = 0 ; j < 4 ; j++){
-                    if(random[i] == input[j]){
-                        dung++;
-                    }
-                }
-                if(random[i] == input[i]){
-                    vitri++;
-                }
-            }
-            
-            var nameCell = newRow.insertCell();
-            var ageCell = newRow.insertCell();
-            var cityCell = newRow.insertCell();
-            nameCell.innerHTML = output;
-            ageCell.innerHTML = dung;
-            cityCell.innerHTML = vitri;
-                
-            if(dung == 4 && vitri == 4){
-                $(".show").removeClass("show");
+});
 
-                alert(random + " ==>> you wwin");
-                $("#lamlai").addClass("show");
-
-            }
-            // console.log(dung);
-        }else{
-            alert("nhập thiếu giá trị!! -_-")
+// sự kiện click cho các class number để nhập số vào input
+$(".number").on("click", function () {
+    var a = $(this).text() + " ";
+    var check = $(this).text()
+    var b = $("#mess").text().slice(0, 6);
+    var set = $("#mess").text().split(" ");
+    console.log(set.indexOf(check))
+    if (set.indexOf(check) !== -1){
+        console.log(b)
+        $("#mess").html(b);
+        return;
+    }else{
+        $("#mess").html(b + a);
+        console.log(b + a)
+        return;
     }
-    });
-    $("#delete").click(function () {
-        var input = $("#mess").text().split(""); 
-        input.splice(-2);
-        $("#mess").html(input.join(""));     
-    });
-}
-$("#lamlai").click(function(){
-    location.reload();
-    // $(".show").removeClass("show");
-    // $(".play").addClass("show");
+});   
 
-})
+//sự kiện click checks dãy số từ input vừa nhập
+$("#submit").click(function () { 
+    var input = $("#mess").text().split(" ");
+    if(input.length > 4){
+        var table = document.getElementById("myTable").getElementsByTagName("tbody")[0];
+        var newRow = table.insertRow();
+        var output =  $("#mess").text().split(",");
+        document.getElementById("mess").innerHTML = ""
+        var dung = 0;
+        var vitri = 0;
+        for(i = 0 ; i < 4 ; i++){
+            for(j = 0 ; j < 4 ; j++){
+                if(random[i] == input[j]){
+                    dung++;
+                }
+            }
+            if(random[i] == input[i]){
+                    vitri++;
+            }
+        }
+        var nameCell = newRow.insertCell();
+        var ageCell = newRow.insertCell();
+        var cityCell = newRow.insertCell();
+        nameCell.innerHTML = output;
+        ageCell.innerHTML = dung;
+        cityCell.innerHTML = vitri;
+        if(dung == 4 && vitri == 4){
+            $(".show").removeClass("show");
+            alert(output + " ==>> you wwin");
+            $("#play").addClass("show");
+            return 0;
+        }
+    }
+});
+
+//sự kiện click để xóa phần tử trong mảng input
+$("#delete").click(function () {
+    var input = $("#mess").text().split(""); 
+    input.splice(-2);
+    $("#mess").html(input.join(""));     
+});
+
+//hàm random lấy 4 số trog khoảng từ 1 đến 9
 function ranDomArr(){
     var numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
     var randomNumbers = [];
@@ -84,5 +87,4 @@ function ranDomArr(){
         }
     }
     return randomNumbers;
-  
-  }
+}
